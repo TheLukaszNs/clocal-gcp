f160501ac8e4a23564a3639b79ef6512713afbd4
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2018-11-14 13:07:15 +0100

    Remove token from get.js

--- examples/cloud-memory-store/get.js
+++ examples/cloud-memory-store/get.js
@@ -1,13 +1,9 @@
-var request = require('request');
+const request = require('request');
 
-var options = {
+const options = {
   method: 'GET',
-  url:
-    'http://localhost:7070/v1beta1/projects/testProject/locations/us-east-1/instances/redisTest123',
-  headers: {
-    'Postman-Token': '8be7f29e-cff0-4b95-a43a-6eb347bc0e37',
-    'Cache-Control': 'no-cache',
-  },
+  url: 'http://localhost:7070/v1beta1/projects/testProject/locations/us-east-1/instances/redisTest123',
+  headers: {  'Cache-Control': 'no-cache'  },
 };
 
 request(options, function(error, response, body) {
